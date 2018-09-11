bc911f0bccb72490b3c33f56e8c94a9abfcf51d4
nenjotsu <renenjo.valente@ubidy.com>
2018-09-11 23:55:05 +0800

    add gpu-info

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const isOnline = require('is-online');
-const wintools = require('wintools');
-
 const every30mins = 1800000;
-// const every3s = 3000;
+const isWin = process.platform === 'win32';
+const every3s = 3000;
 
 setInterval(function() {
   isOnline().then(online => {
     console.log(online);
-    if (!onLine) {
-      wintools.shutdown.restart();
+    if (!online) {
+      if (isWin) {
+        const wintools = require('wintools');
+        const gpuInfo = require('gpu-info');
+
+        gpuInfo().then(function(data) {
+          console.log('GPUS:', data);
+        });
+        wintools.shutdown.restart();
+      }
     }
   });
-}, every30mins);
+}, every3s);
